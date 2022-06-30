71d560c5fd6b04cac6d52034acb2b80c47ebf1f2
Gordon Murray <gordon@gordonmurray.com>
2022-06-30 22:17:29 +0100

    Change password label

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -1,8 +1,8 @@
 resource "aws_secretsmanager_secret" "example" {
   kms_key_id              = aws_kms_key.secrets_manager.key_id
-  name                    = "rds_admin"
+  name                    = "rds_admin_password"
   description             = "RDS Admin password"
-  recovery_window_in_days = 14
+  recovery_window_in_days = 0
 
   tags = {
     Name = var.default_tag
