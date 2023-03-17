7ca6a5d788dd0d3d8639e2cddca61be819f92b87
Gordon Murray <gordon@gordonmurray.com>
2023-03-17 19:27:12 +0000

    Interpolate variables in to the data generator script

--- files/user_data.sh
+++ files/user_data.sh
@@ -11,6 +11,12 @@ sed -i "s/\[DATABASE_PASSWORD\]/${database_password}/g" /home/ubuntu/connector.j
 sed -i "s/\[DATABASE_SCHEMA\]/${database_schema}/g" /home/ubuntu/connector.json
 sed -i "s/\[DATABASE_TABLE\]/${database_table}/g" /home/ubuntu/connector.json
 
+# Populate the data generator
+sed -i "s/\[DATABASE_HOST\]/${database_host}/g" /home/ubuntu/generate_data.sh
+sed -i "s/\[DATABASE_USERNAME\]/${database_username}/g" /home/ubuntu/generate_data.sh
+sed -i "s/\[DATABASE_PASSWORD\]/${database_password}/g" /home/ubuntu/generate_data.sh
+sed -i "s/\[DATABASE_SCHEMA\]/${database_schema}/g" /home/ubuntu/generate_data.sh
+
 # Populate the apicurio registry start script
 sed -i "s/\[KAFKA_BROKERS\]/${brokers}/g" /home/ubuntu/start_registry.sh
 
