46aa0d25aa3ec42e0804aa8bb1f5d2ef22bb031a
Gordon Murray <gordon@gordonmurray.com>
2023-03-18 13:21:59 +0000

    encryption on the log group

--- cloudwatch_log_group.tf
+++ cloudwatch_log_group.tf
@@ -1,5 +1,6 @@
 resource "aws_cloudwatch_log_group" "kafka_broker_logs" {
-  name = "msk_broker_logs"
+  name       = "msk_broker_logs"
+  kms_key_id = aws_kms_key.cloudwatch_key.arn
 
   retention_in_days = 1
 
