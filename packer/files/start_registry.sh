23d8b393fd3225960434f1969ed5486359cbf5dd
Gordon Murray <gordon@gordonmurray.com>
2023-03-19 19:24:21 +0000

    Needs permission

--- packer/files/start_registry.sh
+++ packer/files/start_registry.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # This starts the apicura schema registry
-java -Dregistry.kafka.common.bootstrap.servers=[KAFKA_BROKERS] -jar apicurio-registry-storage-kafkasql-2.4.1.Final-runner.jar
+sudo java -Dregistry.kafka.common.bootstrap.servers=[KAFKA_BROKERS] -jar apicurio-registry-storage-kafkasql-2.4.1.Final-runner.jar
